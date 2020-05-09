d97d85be99a99960416140f94d2881d6865e6559
Dusan Marin <dusan.marin@live.com>
2020-05-09 16:21:08 +0200

    remove cors

--- PackagingChallenge.WebAPI/Startup.cs
+++ PackagingChallenge.WebAPI/Startup.cs
@@ -28,10 +28,6 @@ namespace PackagingChallenge.WebAPI
         {
             services.AddTransient<IPackagingService, PackagingService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddCors(c =>
-            {
-                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
